1fb4eec3439e4f33595fc64d77d913c3834d938d
ArchNoah <noah.buergler@gmail.com>
2024-06-02 03:55:24 +0200

    added create editfile fallback when none exists

--- edit/src/root_command.sh
+++ edit/src/root_command.sh
@@ -1,6 +1,6 @@
 template=${args[--template]}
 
-if is not empty "$template"; then
+function create_template() {
     echo "Creating a template from $template"
     if is equal "$template" "rust"; then
         echo "./src/main.rs" > 'editfile'
@@ -10,14 +10,20 @@ if is not empty "$template"; then
         echo "./src/bashly.yml" >> 'editfile'
     else
         echo "not yet implemented..."
+        return 1
     fi
-    return 0
+}
+
+if is not empty "$template"; then
+    exit 0
 fi
 
 if is existing "editfile"; then
-    joinedlines=$(tr '\n' ' '< "editfile")
+    joinedlines=$(tr '\n' ' ' < "editfile")
     $EDITOR $joinedlines
 else
     echo "no editfile found..."
+    gum confirm 'create one from a template?' && \
+        template=$(gum choose 'rust' 'bashly') create_template && exit 0
     exit 1
 fi
