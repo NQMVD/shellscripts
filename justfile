2fa90f0b11797fbccde8ae6ec728a06efd14ad5e
noah <noahbuergler@proton.me>
2024-07-28 20:40:44 +0200

    feat ✨(zel, newupdate, whatclip): new stuff
    Signed-off-by: noah <noahbuergler@proton.me>

--- justfile
+++ justfile
@@ -1,11 +1,14 @@
+# new bashly project
 new name:
     @gum log -sl info "Creating" "project" {{ name }}
     mkdir {{ name }} && cd {{ name }} && bashly init --minimal
 
+# create executable copy in local bin
 cec file:
     chmod +x {{file}}
     sudo cp "{{file}}" "/usr/local/bin/`basename {{file}} .sh`"
     @eza /usr/local/bin -1
 
+# show diff to last git update
 diff:
     @git diff --name-only HEAD~1 HEAD
---
+++ whatclip.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Define popular clipboard tools for each session type
+case "$XDG_SESSION_TYPE" in
+  x11)
+    # X11 session type
+    clipboard_tools=("xclip" "xsel" "klipper")
+    ;;
+  wayland)
+    # Wayland session type
+    clipboard_tools=("wl-clipboard" "wl-copy" "wl-paste")
+    ;;
+  *)
+    # Unknown session type, default to X11 tools
+    clipboard_tools=("xclip" "xsel" "klipper")
+    ;;
+esac
+
+# Check if each clipboard tool is installed using which
+for tool in "${clipboard_tools[@]}"; do
+  if which "$tool" &> /dev/null; then
+    echo "$tool"
+    exit 0
+  fi
+done
+
+# If no clipboard tool is detected, print an empty string
+echo "NONE"
